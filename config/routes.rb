3354769d14130cb97859f6d1aea0c680b920263d
anhcnh-1028 <nguyen.hoang.anhc@sun-asterisk.com>
2020-07-08 14:33:21 +0700

    Chap3: Static pages

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+
+  scope "(:locale)", locale: /en|vi/ do
+    resources :microposts
+    resources :users
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
